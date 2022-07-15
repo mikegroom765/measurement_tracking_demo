cb948bdac027725468cf9b086cd64111fc8d9cb6
Mike Groom <Mike.Groom@jacobs.com>
2022-07-15 17:41:51 +0100

    ROS stuff

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,24 @@ ENV NVIDIA_VISIBLE_DEVICES \
 ENV NVIDIA_DRIVER_CAPABILITIES \
     ${NVIDIA_DRIVER_CAPABILITIES:+$NVIDIA_DRIVER_CAPABILITIES,}graphics
 
-RUN apt-get update && apt-get install -y \
+# Change the default shell to Bash
+SHELL [ "/bin/bash" , "-c" ]
+
+RUN apt-get update && apt-get install -y git && apt-get install -y \
     ros-noetic-rtabmap \
     ros-noetic-rtabmap-ros \
     ros-noetic-realsense2-camera
+
+# Create a Catkin workspace and clone demo code
+RUN source /opt/ros/noetic/setup.bash \
+    && mkdir -p /measurement_demo_ws/src \
+    && cd /measurement_demo_ws/src \
+    && catkin_init_workspace \
+    && git clone https://github.com/mikegroom765/measurement_tracking_demo.git
+
+# Build the Catkin workspace and ensure it's sourced
+
+RUN source /opt/ros/noetic/setup.bash \
+    && cd measurement_demo_ws \
+    && catkin_make
+RUN echo "source /measurement_demo_ws/devel/setup.bash" >> ~/.bashrc
---
+++ src/poseUserDataUsage.cpp
@@ -0,0 +1,87 @@
+#include <ros/ros.h>
+#include <ros/publisher.h>
+#include <ros/subscriber.h>
+#include <rtabmap_ros/MsgConversion.h>
+#include <rtabmap_ros/UserData.h>
+#include <visualization_msgs/Marker.h>
+#include <geometry_msgs/Pose.h>
+#include <nav_msgs/Odometry.h>
+#include <iostream>
+
+class CameraPoseSub
+{
+public:
+    void odomCallback(const nav_msgs::Path::ConstPtr& msg);
+    double pos_x;
+    double pos_y;
+    double pos_z;
+    double or_x;
+    double or_y;
+    double or_z;
+    double or_w;
+};
+
+void CameraPoseSub::odomCallback(const nav_msgs::Path::ConstPtr& msg){
+
+    self.pos_x = msg->pose.pose.position.x;
+    self.pos_y = msg->pose.pose.position.y;
+    self.pos_z = msg->pose.pose.position.z;
+    self.or_x = msg->pose.pose.orientation.x;
+    self.or_y = msg->pose.pose.orientation.y;
+    self.or_z = msg->pose.pose.orientation.z;
+    self.or_w = msg->pose.pose.orientation.w;
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "marker_pose_pub")
+
+    ros::NodeHandle nh_0;
+    ros::NodeHandle nh_1;
+
+    CameraPoseSub cps;
+    ros::Subscriber cameraPoseSub = nh_0.subscribe("rtabmap/odom", 10, &CameraPoseSub::odomCallback, &cps);
+    ros::Publisher vis_pub = nh_1.advertise<visualization_msgs::Marker>( "visualization_marker", 0);
+
+    std::string input;
+    int marker_id = 0;
+
+    while(ros::ok())
+    {
+        ros::Time stamp = ros::Time:now();
+
+        input = "";
+        cin >> input;
+        
+        if (input == " ")
+        {
+            visualization_msgs::Marker marker;
+            marker.header.frame_id = "base_link";
+            marker.header.stamp = ros::Time();
+            marker.ns = "my_namespace";
+            marker.id = marker_id;
+            marker.type = visualization_msgs::Marker::ARROW;
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.position.x = cps.pos_x;
+            marker.pose.position.y = cps.pos_y;
+            marker.pose.position.z = cps.pos_z;
+            marker.pose.orientation.x = cps.or_x;
+            marker.pose.orientation.y = cps.or_y;
+            marker.pose.orientation.z = cps.or_z;
+            marker.pose.orientation.w = cps.or_w;
+            marker.scale.x = 1;
+            marker.scale.y = 0.1;
+            marker.scale.z = 0.1;
+            marker.color.a = 1.0;
+            marker.color.r = 0.0;
+            marker.color.g = 1.0;
+            marker.color.b = 0.0;
+            vis_pub.publish( marker );
+            marker_id += 1;
+        }
+        
+
+        ros::spinOnce();
+    }
+    return 0;
+}
